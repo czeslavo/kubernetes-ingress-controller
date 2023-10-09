4decf6f0006d8300990b09d57c2b9354421a964d
Grzegorz Burzyński <czeslavo@gmail.com>
2023-10-09 20:31:44 +0200

    gke, istio

--- scripts/test-deps/main.go
+++ scripts/test-deps/main.go
@@ -12,8 +12,9 @@ import (
 // DependenciesFile is a map of test levels and their dependencies.
 type DependenciesFile map[string]Dependencies
 
-// Dependencies is a map of dependencies and their versions.
-type Dependencies map[string][]string
+// Dependencies is a map of dependencies and their versions OR
+// a slice of matrix entries that can be used in GitHub Actions matrix directly.
+type Dependencies any
 
 // This program reads `.github/test_dependencies.yaml` file, extracts a requested dependency's versions
 // from it and prints it to stdout as a JSON array that can be used in GitHub Actions matrix.
@@ -45,19 +46,36 @@ func main() {
 		exitWithErr(fmt.Errorf("test level %s not found", testLevel))
 	}
 
-	versions, ok := testLevelDeps[dependency]
-	if !ok {
-		exitWithErr(fmt.Errorf("dependency %s.%s not found", testLevel, dependency))
+	switch deps := testLevelDeps.(type) {
+	case []string:
+		// If the test level is a slice of matrix entries, print it as a JSON array and exit.
+		err = json.NewEncoder(os.Stdout).Encode(deps)
+		exitIfErr(err)
+		os.Exit(0)
+	case map[string]interface{}:
+		// If the test level is a map of dependencies and their versions, continue.
+		versions, ok := deps[dependency]
+		if !ok {
+			exitWithErr(fmt.Errorf("dependency %s.%s not found", testLevel, dependency))
+		}
+
+		vs, ok := versions.([]any)
+		if !ok {
+			exitWithErr(fmt.Errorf("dependency %s.%s is of unsupported type: %T", testLevel, dependency, versions))
+		}
+
+		// If only the latest, use only the latest version...
+		if latest {
+			vs = []any{vs[len(vs)-1]}
+		}
+
+		// Print the versions as a JSON array.
+		err = json.NewEncoder(os.Stdout).Encode(vs)
+		exitIfErr(err)
+	default:
+		exitWithErr(fmt.Errorf("test level %q is of unsupported type: %T", testLevel, testLevelDeps))
 	}
 
-	// If only the latest, use only the latest version...
-	if latest {
-		versions = []string{versions[len(versions)-1]}
-	}
-
-	// Print the versions as a JSON array.
-	err = json.NewEncoder(os.Stdout).Encode(versions)
-	exitIfErr(err)
 }
 
 func exitIfErr(err error) {
